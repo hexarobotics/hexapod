91d9c667dccf026f44d3330c7106a6a696917366
miguel.gomez <miguel.gomez.diaz.93@gmail.com>
2024-06-29 21:11:33 +0200

    adapt code

--- Inc/pca9685.h
+++ Inc/pca9685.h
@@ -1,24 +1,10 @@
-#pragma once
 
-#if (defined STM32L011xx) || (defined STM32L021xx) || \
-	(defined STM32L031xx) || (defined STM32L041xx) || \
-	(defined STM32L051xx) || (defined STM32L052xx) || (defined STM32L053xx) || \
-	(defined STM32L061xx) || (defined STM32L062xx) || (defined STM32L063xx) || \
-	(defined STM32L071xx) || (defined STM32L072xx) || (defined STM32L073xx) || \
-	(defined STM32L081xx) || (defined STM32L082xx) || (defined STM32L083xx)
-#include "stm32l0xx_hal.h"
-#elif defined (STM32L412xx) || defined (STM32L422xx) || \
-	defined (STM32L431xx) || (defined STM32L432xx) || defined (STM32L433xx) || defined (STM32L442xx) || defined (STM32L443xx) || \
-	defined (STM32L451xx) || defined (STM32L452xx) || defined (STM32L462xx) || \
-	defined (STM32L471xx) || defined (STM32L475xx) || defined (STM32L476xx) || defined (STM32L485xx) || defined (STM32L486xx) || \
-    defined (STM32L496xx) || defined (STM32L4A6xx) || \
-    defined (STM32L4R5xx) || defined (STM32L4R7xx) || defined (STM32L4R9xx) || defined (STM32L4S5xx) || defined (STM32L4S7xx) || defined (STM32L4S9xx)
-#include "stm32l4xx_hal.h"
-#else
-#error Platform not implemented
-#endif
+#ifndef __PCA9685_H
+#define __PCA9685_H
 
+//#include "stm32f4xx_hal_i2c.h"
 #include <stdbool.h>
+#include <stdint.h>
 
 #ifndef PCA9685_I2C_TIMEOUT
 #define PCA9685_I2C_TIMEOUT 1
@@ -112,3 +98,5 @@ bool pca9685_set_channel_pwm_times(pca9685_handle_t *handle, unsigned channel, u
  * @return True on success, false otherwise.
  */
 bool pca9685_set_channel_duty_cycle(pca9685_handle_t *handle, unsigned channel, float duty_cycle, bool logarithmic);
+
+#endif /* __PCA9685_H */
